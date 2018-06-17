84ff8b0a2f20e3f894dfc9070db73da54d9fb639
Sean Conrad <seanconrad123@gmail.com>
2018-06-17 00:50:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect (array)
+  i = 0
+  result = []
+  while i < array.length
+    result << yield(array[i]) if block_given?
+    i += 1
+  end
+  result
+end
 
+array = ["ruby", "javascript", "python", "objective-c"]
 
+#my_collect(array)
