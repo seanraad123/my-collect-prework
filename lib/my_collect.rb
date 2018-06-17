10a0067614735b4ff5db78e4ee5cf1cc6629a147
Sean Conrad <seanconrad123@gmail.com>
2018-06-17 00:50:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect (array)
   i = 0
-  result
+  result = []
   while i < array.length
     result << yield(array[i]) if block_given?
     i += 1
   end
+  result
 end
 
 array = ["ruby", "javascript", "python", "objective-c"]
