4ba9df3c9c250f15fabe491272e2265a35edb196
Sean Conrad <seanconrad123@gmail.com>
2018-06-17 00:38:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect (array)
   i = 0
   while i > array.length
-    yield(array.upcase) #if block_given?
+    yield(array.upcase) if block_given?
     i += 1
   end
-  array
+  puts array
 end
 
 array = ["ruby", "javascript", "python", "objective-c"]
